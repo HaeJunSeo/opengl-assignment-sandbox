3de399ef7d1f618ccbefc2fd855185f35bb375d7
Gumball12 <kan02134@gmail.com>
2019-03-25 18:28:31 +0900

    Init: pixijs

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const env = {
+  width: 150, height: 100,
+  pixelSize: 5
+};
+
+let app;
+
 require([
   'https://cdnjs.cloudflare.com/ajax/libs/async/2.6.1/async.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/pixi.js/4.8.6/pixi.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js'
-], (async, _) => {
+], (async, PIXI, _) => {
   async.waterfall([
-    init
+    init,
+    renderGrid,
+    render
   ], err => {
     if (err) {
       throw err;
@@ -13,7 +23,25 @@ require([
   });
 
   function init (cb) {
+    // disable antialias
+    PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+    app = new PIXI.Application({
+      width: env.width * env.pixelSize, height: env.height * env.pixelSize,
+      backgroundColor: 0xffffff
+    });
+
+    document.body.appendChild(app.view);
+
+    cb();
+  }
+
+  function renderGrid (cb) {
     //
+    cb();
+  }
+
+  function render (cb) {
     cb();
   }
 });
