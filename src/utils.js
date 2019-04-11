e97a34eca8bb05ac83e380d0963819d5e70685d2
Gumball12 <kan02134@gmail.com>
2019-04-11 21:59:30 +0900

    Feat: Add dot product, transpose matrix

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@
 define([
   env.cdn['lodash']
 ], (_) => {
-  return {
+  const functions = {
     /**
      * 1D -> 2D
      * 
@@ -39,6 +39,57 @@ define([
       console.log(`⏱ ${cb.name} >> ${window.performance.now() - s}ms`);
 
       return ret;
+    },
+
+    /**
+     * Vector to Matrix
+     * 
+     * @param v vector or matrix
+     */
+    vec2Mat (v) {
+      if (!_.isArray(v[0])) {
+        v = [v];
+      }
+
+      return v;
+    },
+
+    /**
+     * dot product
+     * 
+     * @param m1 vector or matrix
+     * @param m2 vector or matrix
+     * @return {[[...Number]]} 
+     */
+    dot (m1, m2) {
+      m1 = functions.vec2Mat(m1);
+      m2 = functions.transpose(functions.vec2Mat(m2));
+
+      if (m1[0].length !== m2[0].length) {
+        throw new Error('invalid operand');
+      }
+
+      return _.reduce(m1, (r, v1) =>
+        r.concat([_.reduce(m2, (r, v2) =>
+          r.concat([_.reduce(v1, (r, val, ind) =>
+            r + val * v2[ind], 0)]), [])]), []);
+    },
+
+    /**
+     * transpose of a matrix
+     * 
+     * @param {[[...Number]]} mat matrix
+     */
+    transpose (mat) {
+      mat = functions.vec2Mat(mat);      
+      const len = mat[0].length;
+
+      return _.reduce(_.flatten(mat), (r, el, ind) => {
+        r[ind % len].push(el);
+        return r;
+      }, _.map(_.range(len), () => []));
     }
-  }
+  };
+
+  return functions;
 });
